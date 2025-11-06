08973073614bc187ebe295e4023cbe6202c585a0
Anil Bamnote <anilbamnote21@gmail.com>
2025-11-06 09:56:43 +0530

    s3-backend-stage-added | AB

--- pipeline.groovy
+++ pipeline.groovy
@@ -52,6 +52,14 @@ pipeline {
                 // sh '/opt/maven/bin/mvn sonar:sonar  -Dsonar.projectKey=studentapp -Dsonar.host.url=http://172.31.22.154:9000 -Dsonar.login=05d72f8340ad25c99e810ea637aa4eb0f7c87745'
             }
         }
+         stage('Quality-gate') {
+            steps {
+                timeout(10) {
+               
+            }
+                waitForQualityGate true
+            }
+        }
         stage('Deploy') {
             steps {
                 echo "deploy-sucess"
